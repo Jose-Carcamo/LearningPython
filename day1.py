4732d3031232b28a0b62b03c32bb40fae0b153bd
Jose-Carcamo <josecarcamo21@icloud.com>
2025-12-27 16:41:00 -0500

    Day 1: Basic input/output program

---
+++ day1.py
@@ -0,0 +1,4 @@
+age = int(input("How old are you? "))
+name = input("What is your name? ")
+hobby = input("What is your favorite hobby? ")
+print(f"Hello {name}! You are {age} years old and your favorite hobby is {hobby}.")
